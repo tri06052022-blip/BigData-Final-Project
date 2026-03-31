113005a4c2560532fac6dd75fd5f23a428e85554
Lê Tuấn Thiên Chương <thienchuong6859@gmail.com>
2026-03-31 10:45:52 +0700

    Delete Models/Read best_classification_model_joblib.py

--- Models/Read best_classification_model_joblib.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-# Thêm chữ r trước đường dẫn để Python hiểu đây là Raw string
-model = joblib.load(r'D:\học\BIG DATA\Cuối Kỳ\best_classification_model.joblib') 
-
-print(model)
-print("Thông số mô hình:", model.get_params())
